fc76bea60635cd40963d16e02c4afdf126fdbfa8
Luis Gustavo <luisgustavolf@gmail.com>
2021-03-31 10:38:04 -0300

    transfer dialog

--- src/pages/home/transferDialog/index.tsx
+++ src/pages/home/transferDialog/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogContent, DialogTitle } from '@material-ui/core'
+import { Button, Dialog, DialogContent, DialogTitle, TextField, Typography } from '@material-ui/core'
 import DialogActions from '@material-ui/core/DialogActions'
 import * as React from 'react'
 import { useTransferDialog } from './hook'
@@ -20,6 +20,12 @@ export function TransferDialog(props: TransferDialogProps) {
     
     // ---------------------------------------------
     // Transformations
+
+    const sourceBalance = accounts.sourceAccount?.balance && accounts.sourceAccount?.balance.toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    })
+
     // ---------------------------------------------
     // Render
 
@@ -28,11 +34,26 @@ export function TransferDialog(props: TransferDialogProps) {
             open={open}
         >
             <DialogTitle>
-                Transferir {accounts.sourceAccount?.name} para {accounts.targetAccount?.name}
+                Transferência
             </DialogTitle>
-            <DialogContent>
-                
+            <DialogContent className={'dd-transfer-dialog-content'}>
+                <Typography variant="caption">De</Typography>
+                <Typography variant="h4">{accounts.sourceAccount?.name}</Typography>
+
+                <Typography variant="caption">Para</Typography>
+                <Typography variant="h4">{accounts.targetAccount?.name}</Typography>
                
+                <Typography variant="caption">Saldo em conta</Typography>
+                <Typography variant="h4">{sourceBalance} {accounts.sourceAccount?.currency}</Typography>
+
+                <TextField
+                    style={{marginTop: 20}}
+                    label="Valor à transferir"
+                    value={0}
+                    variant="outlined"
+                    helperText={`0 ${accounts.targetAccount?.currency}`}
+                    fullWidth
+                />
 
             </DialogContent>
             <DialogActions >
