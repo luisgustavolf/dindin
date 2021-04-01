1859816c320a6d3f1f70ce57ea5dc38b2dfb5068
Luis Gustavo <luisgustavolf@gmail.com>
2021-03-31 22:07:54 -0300

    added logo

---
+++ src/components/environment/floatingLogo/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+import './styles.scss'
+
+export function FloatingLogo() {
+
+    // ---------------------------------------------
+    // Transformations
+    // ---------------------------------------------
+    // Render
+
+    return (
+        <div className={'dd-floating-logo'}>
+            #Dindin
+        </div>
+    )
+}
--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { Route, Switch } from "react-router-dom";
+import { FloatingLogo } from '../components/environment/floatingLogo';
 import { HomePage } from './home';
 import { PlaygroundPage } from './playground';
 
 export function Routes() {
     return (
         <React.Fragment>
+            <FloatingLogo/>
             <Switch>
                 <Route path={'/playground'} component={PlaygroundPage}/>
                 <Route path={'/'} component={HomePage}/>
